fd104a8d1d46ece3ca1a27f6c661b5e158213909
Ramon Wijnands <ramon@ruvu.nl>
2018-03-06 21:51:13 +0100

    Link to the offical soem library

--- include/ethercat_interface/ethercat_includes.h
+++ include/ethercat_interface/ethercat_includes.h
@@ -2,17 +2,17 @@
 #define __ETHERCAT_INCLUDES_H
 
 
-#include "ethercat_soem/ethercattype.h"
-#include "ethercat_soem/nicdrv.h"
-#include "ethercat_soem/osal.h"
-#include "ethercat_soem/oshw.h"
-#include "ethercat_soem/ethercatmain.h"
-#include "ethercat_soem/ethercatdc.h"
-#include "ethercat_soem/ethercatcoe.h"
-#include "ethercat_soem/ethercatfoe.h"
-#include "ethercat_soem/ethercatsoe.h"
-#include "ethercat_soem/ethercatconfig.h"
-#include "ethercat_soem/ethercatprint.h"
+#include "soem/ethercattype.h"
+#include "soem/nicdrv.h"
+#include "soem/osal.h"
+#include "soem/oshw.h"
+#include "soem/ethercatmain.h"
+#include "soem/ethercatdc.h"
+#include "soem/ethercatcoe.h"
+#include "soem/ethercatfoe.h"
+#include "soem/ethercatsoe.h"
+#include "soem/ethercatconfig.h"
+#include "soem/ethercatprint.h"
 
 //#include "ethercat_interface/io_interfaces.h"
 //#include "ethercat_interface/slave_driver.h"
