cf90359f3821507565c8fae210d0cb5eb2501bd0
Janno Lunenburg <jannolunenburg@gmail.com>
2018-04-03 23:01:13 +0200

    wip()Added analog input

---
+++ src/analog_input.h
@@ -0,0 +1,52 @@
+#ifndef ANALOG_INPUT_H
+#define ANALOG_INPUT_H
+
+/**
+ * @brief The Analog Input
+ */
+template <typename T>
+class AnalogInput : public ReadInterface
+{
+public:
+
+    /**
+     * @brief AnalogInput Constructor.
+     * @param data_ptr pointer to the location where the data should be written
+     */
+    AnalogInput(T *data_ptr) : data_ptr_(data_ptr) {}
+
+    /**
+     * @brief read reads the analog input
+     * @return the analog input value
+     */
+    double read()
+    {
+        T int_value = *data_ptr_;
+
+//        ROS_DEBUG_THROTTLE(1.0, "Encoder read value: %u", new_value);
+//        if( (previous_value_ - new_value) > encoder_max_ / 2)
+//        {
+//            revolution_overflows_++;
+//            ROS_DEBUG_THROTTLE(1.0, "Incrementing overflow to %i", revolution_overflows_);
+//        }
+//        else if ((previous_value_ - new_value) < -1 * (double)encoder_max_/2)
+//        {
+//            revolution_overflows_--;
+//            ROS_DEBUG_THROTTLE(1.0, "Decrementing overflow to %i", revolution_overflows_);
+//        }
+
+//        previous_value_ = new_value;
+
+//        int int_position = revolution_overflows_ * encoder_max_ + new_value;
+        double value = static_cast<double>(int_value);
+        ROS_DEBUG_THROTTLE(1.0, "Analog input: %.1f", value);
+
+        return value;
+    }
+
+private:
+    T* data_ptr_;
+
+}; // End of class AnalogInput
+
+#endif // ANALOG_INPUT_H
--- src/tuees030.cpp
+++ src/tuees030.cpp
@@ -1,4 +1,5 @@
 #include "./tuees030.h"
+#include "analog_input.h"
 #include "encoder.h"
 
 TUeES030::TUeES030(ec_slavet *slave) : EtherCatDriver(slave)
@@ -8,10 +9,53 @@ TUeES030::TUeES030(ec_slavet *slave) : EtherCatDriver(slave)
 
     // Input channels
     in_tueEthercatMemoryt* input_struct_ptr = (in_tueEthercatMemoryt*)(ec_slave_->inputs);
-    // ToDo: mstate1
+    // ToDo: mstate1 [0]
     inputs_[1] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_1);
-    inputs_[2] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_2);
-    inputs_[3] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_3);
+    inputs_[2] = std::make_shared<AnalogInput<int16> >(&input_struct_ptr->current_1);
+    // ToDo: mstate2 [3]
+    inputs_[4] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_2);
+    inputs_[5] = std::make_shared<AnalogInput<int16> >(&input_struct_ptr->current_2);
+    // ToDo: mstate3 [6]
+    inputs_[7] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_3);
+    inputs_[8] = std::make_shared<AnalogInput<int16> >(&input_struct_ptr->current_3);
+    //    digital_in_t digital_in;                    // digital input 8 bits [10, 11, 12, 13, 14, 15, 16, 17]
+    //    uint16      calipher_1;			// calipher 1 (1 bit = 0.01 mm) [18]
+    //    uint16      calipher_2;			// calipher 2 (1 bit = 0.01 mm) [19]
+    inputs[20] = std::make_shared<AnalogInput<uint16> >(&input_struct_ptr->force_1);
+    inputs[21] = std::make_shared<AnalogInput<uint16> >(&input_struct_ptr->force_2);
+    inputs[22] = std::make_shared<AnalogInput<uint16> >(&input_struct_ptr->force_3);
+
+
+
+
+//    uint8       mstate1;			// motor state 1
+//    uint32      encoder_1;			// Encoder 1
+//    uint32      timestamp1;			// Time stamp encoder 1 (Only changes, if encoder changes)
+//    int16       velocity1;			// Velocity encoder 1; 1 bit=0.1 rad/s; depending on parameters on slave
+//    int16       current_1;			// current on PWM 1 (1 bit = 1 mA)
+//    uint8       mstate2;			// motor state 2
+//    uint32      encoder_2;			// Encoder 2
+//    uint32      timestamp2;			// Time stamp encoder 2 (Only changes, if encoder changes)
+//    int16       velocity2;			// Velocity encoder 2; 1 bit=0.1 rad/s; depending on parameters on slave
+//    int16       current_2;			// current on PWM 2 (1 bit = 1 mA)
+//    uint8       mstate3;                        // motor state 3
+//    uint32      encoder_3;			// Encoder 3
+//    uint32      timestamp3;			// Time stamp encoder 3 (Only changes, if encoder changes)
+//    int16       velocity3;			// Velocity encoder 3; 1 bit=0.1 rad/s; depending on parameters on slave
+//    int16       current_3;			// current on PWM 2 (1 bit = 1 mA)
+//    digital_in_t digital_in;                    // digital input 8 bits
+//    uint16      calipher_1;			// calipher 1 (1 bit = 0.01 mm)
+//    uint16      calipher_2;			// calipher 2 (1 bit = 0.01 mm)
+//    uint16      force_1;			// Analog ADC value of force sensor input 1
+//    uint16      force_2;			// Analog ADC value of force sensor input 2
+//    uint16      force_3;			// Analog ADC value of force sensor input 3
+//    uint16      position_1;			// Analog ADC value of position sensor 1
+//    uint16      position_2;			// Analog ADC value of position sensor 2
+//    uint16      position_3;			// Analog ADC value of position sensor 3
+//    uint16      spare_ai_1;			// Spare analog in 1
+//    uint16      spare_ai_2;			// Spare analog in 2
+//    uint16      linevoltage;        // 2500 bits = 25V; 1 bit = 0,01V
+//    uint16      time_stamp;
 
     // Output channels
 
