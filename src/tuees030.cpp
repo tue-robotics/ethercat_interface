2830b812ce6fd8abac20ac82592594f56b36cfc4
Janno Lunenburg <jannolunenburg@gmail.com>
2018-04-03 22:15:04 +0200

    wip(tuees030)Added encoders

--- src/tuees030.cpp
+++ src/tuees030.cpp
@@ -1,4 +1,5 @@
 #include "./tuees030.h"
+#include "encoder.h"
 
 TUeES030::TUeES030(ec_slavet *slave) : EtherCatDriver(slave)
 {
@@ -8,7 +9,9 @@ TUeES030::TUeES030(ec_slavet *slave) : EtherCatDriver(slave)
     // Input channels
     in_tueEthercatMemoryt* input_struct_ptr = (in_tueEthercatMemoryt*)(ec_slave_->inputs);
     // ToDo: mstate1
-//    channels_[1] = std::make_shared<Encoder>(&input_struct_ptr->encoder_1);
+    inputs_[1] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_1);
+    inputs_[2] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_2);
+    inputs_[3] = std::make_shared<Encoder<uint32> >(&input_struct_ptr->encoder_3);
 
     // Output channels
 
