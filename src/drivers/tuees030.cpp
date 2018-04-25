8b8e54f4aca32d60f7b133f5b673d163ef9713a6
Janno Lunenburg <jannolunenburg@gmail.com>
2018-04-25 09:52:16 +0200

    wip(tuees030)Added MotorCmds, not yet tested

--- src/drivers/tuees030.cpp
+++ src/drivers/tuees030.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include "ethercat_interface/ethercat_includes.h"
 #include "./tuees030.h"
 #include "../io_interfaces/analog_input.h"
@@ -5,6 +6,77 @@
 #include "../io_interfaces/digital_input.h"
 #include "../io_interfaces/digital_output.h"
 #include "../io_interfaces/encoder.h"
+
+
+/**
+ * @brief The MotorCommand class Interface for TUEes030 MotorCommands. This is specific
+ * for the TUes030 driver
+ */
+class MotorCommand: public WriteInterface
+{
+public:
+
+    /**
+     * @brief MotorCommand Constructor
+     * @param name of this interface. This is convenient to indicate the
+     * physical connection which this interface represents
+     * @param data_ptr pointer to the location where the data should be written
+     * @param motor_id: identifies for which motor this command is intended (1, 2 or 3)
+     */
+    MotorCommand(std::string name, std::bitset<8> *data_ptr, unsigned int motor_id) :
+        data_ptr_(data_ptr), WriteInterface(name), position_(2)
+    {
+      // Check input
+      if (motor_id != 1 && motor_id != 2 && motor_id != 3 )
+      {
+        throw std::invalid_argument("Motor id must be 1, 2 or 3");
+      }
+
+      // ToDo: make nice
+      // Set first bit
+      if (motor_id == 1 or motor_id == 3)
+      {
+        data_ptr_->set(0, true);
+      }
+      else
+      {
+        data_ptr_->set(0, false);
+      }
+
+      // Set second bit
+      if (motor_id == 3)
+      {
+        data_ptr_->set(1, true);
+      }
+      else
+      {
+        data_ptr_->set(1, false);
+      }
+
+      // Set the 'enabled' bit to false
+      data_ptr_->set(position_, false);
+
+    }  // End of constructor
+
+    /**
+     * @brief write Writes the value in the EtherCAT memory before it is send over the EtherCAT bus
+     * @param value value to write
+     * @return bool indicating success
+     */
+    bool write(double value)
+    {
+        // Write the value
+        data_ptr_->set(position_, bool(value));
+        return true;
+    }
+
+private:
+
+    std::bitset<8>* data_ptr_;
+    unsigned int position_;
+
+};  // End of class MotorCommand
+
 
 TUeES030::TUeES030(std::string name, ec_slavet *slave) : EtherCatDriver(name, slave)
 {
@@ -75,28 +147,29 @@ TUeES030::TUeES030(std::string name, ec_slavet *slave) : EtherCatDriver(name, sl
 
     // Output channels
     out_tueEthercatMemoryt* output_struct_ptr = (out_tueEthercatMemoryt*)(ec_slave_->outputs);
-    // ToDo: mcom1 [0]
     // ToDo: pwm_duty_motor_1 [1]
     // ToDo: ff1; [2]
-    // ToDo: mcom2 [3]
     // ToDo: pwm_duty_motor_2 [4]
-    // ToDo: ff2; [4]
-    // ToDo: mcom3 [5]
-    // ToDo: pwm_duty_motor_3 [6]
-    // ToDo: ff3; [7]
+    // ToDo: ff2; [5]
+    // ToDo: pwm_duty_motor_3 [7]
+    // ToDo: ff3; [8]
+
+    outputs_[0] = std::make_shared<MotorCommand>("MotorCommand1", (std::bitset<8>*)&output_struct_ptr->mcom1, 1);
+    outputs_[3] = std::make_shared<MotorCommand>("MotorCommand2", (std::bitset<8>*)&output_struct_ptr->mcom2, 2);
+    outputs_[6] = std::make_shared<MotorCommand>("MotorCommand3", (std::bitset<8>*)&output_struct_ptr->mcom3, 3);
 
     digital_out_t* digital_out = &output_struct_ptr->digital_out;  // ToDo: check order
     std::bitset<8>* line_output_ptr = (std::bitset<8>*)&digital_out->line;
-    outputs_[8] = std::make_shared<DigitalOutput<8> >("Enable1", line_output_ptr, 0);
-    outputs_[9] = std::make_shared<DigitalOutput<8> >("Enable2", line_output_ptr, 1);
-    outputs_[10] = std::make_shared<DigitalOutput<8> >("SpareDO3", line_output_ptr, 2);
-    outputs_[11] = std::make_shared<DigitalOutput<8> >("SpareDO4", line_output_ptr, 3);
-//    outputs_[12] = std::make_shared<DigitalOutput<8> >("Reserved1", line_output_ptr, 4);  // Do not expose?!
-//    outputs_[13] = std::make_shared<DigitalOutput<8> >("Reserved2", line_output_ptr, 5);  // Do not expose?!
-//    outputs_[14] = std::make_shared<DigitalOutput<8> >("Reserved3", line_output_ptr, 6);  // Do not expose?!
-//    outputs_[15] = std::make_shared<DigitalOutput<8> >("Reserved4", line_output_ptr, 7);  // Do not expose?!
-    outputs_[16] = std::make_shared<AnalogOutput<int16> >("AnalogOut1", &output_struct_ptr->analog_out_1, 12, 0.0, 10.0, -2048);
-    outputs_[17] = std::make_shared<AnalogOutput<int16> >("AnalogOut2", &output_struct_ptr->analog_out_2, 12, 0.0, 10.0, -2048);
+    outputs_[9] = std::make_shared<DigitalOutput<8> >("Enable1", line_output_ptr, 0);
+    outputs_[10] = std::make_shared<DigitalOutput<8> >("Enable2", line_output_ptr, 1);
+    outputs_[11] = std::make_shared<DigitalOutput<8> >("SpareDO3", line_output_ptr, 2);
+    outputs_[12] = std::make_shared<DigitalOutput<8> >("SpareDO4", line_output_ptr, 3);
+//    outputs_[13] = std::make_shared<DigitalOutput<8> >("Reserved1", line_output_ptr, 4);  // Do not expose?!
+//    outputs_[14] = std::make_shared<DigitalOutput<8> >("Reserved2", line_output_ptr, 5);  // Do not expose?!
+//    outputs_[15] = std::make_shared<DigitalOutput<8> >("Reserved3", line_output_ptr, 6);  // Do not expose?!
+//    outputs_[16] = std::make_shared<DigitalOutput<8> >("Reserved4", line_output_ptr, 7);  // Do not expose?!
+    outputs_[17] = std::make_shared<AnalogOutput<int16> >("AnalogOut1", &output_struct_ptr->analog_out_1, 12, 0.0, 10.0, -2048);
+    outputs_[18] = std::make_shared<AnalogOutput<int16> >("AnalogOut2", &output_struct_ptr->analog_out_2, 12, 0.0, 10.0, -2048);
 
 //    uint8       mcom1;              // motor 1 command (0=braked, 1/3=controlled, 2=tristate)
 //    int16       pwm_duty_motor_1;   // current setpoint 1 (1 bit = 1 mA) 6A continiuous current
